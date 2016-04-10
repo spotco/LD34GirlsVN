4f553b331f3818954192b3794cbf3cc056dbe7eb
spotco <mootothemax@gmail.com>
2016-04-10 14:35:53 -0700

    ch2 done

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -16,7 +16,7 @@ public class GameMain : MonoBehaviour {
 	public static bool DEBUG_CONTROLS = true;
 	public static bool MUTE = false;
 	public static bool IGNORE_ITEM_REQ = true;
-	public static int NODE_START_INDEX = 38;
+	public static int NODE_START_INDEX = 43;
 	
 	public interface Modal {
 		void i_initialize(GameMain game);
@@ -41,8 +41,6 @@ public class GameMain : MonoBehaviour {
 	public int _affinity;
 	
 	/*
-mana check kurumi okay
-		
 	TODO--
 	script edits
 	SPAnalytics
@@ -50,6 +48,8 @@ mana check kurumi okay
 	end to title UIs
 	credits
 	script node positioning always on screen for mobile
+	
+	MANA child photo
 	
 	sfx load all
 	*/
