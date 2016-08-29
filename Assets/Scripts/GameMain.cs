386962b0bdd621bc11d3b171b130a7fd3a0b1c82
spotco <mootothemax@gmail.com>
2016-08-28 22:37:16 -0700

    new story

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -16,7 +16,7 @@ public class GameMain : MonoBehaviour {
 	public static bool DEBUG_CONTROLS = true;
 	public static bool MUTE = true;
 	public static bool IGNORE_ITEM_REQ = true;
-	public static int NODE_START_INDEX = 0;
+	public static int NODE_START_INDEX = 1;
 	public static bool SKIP_TITLE = true;
 	
 	public interface Modal {
